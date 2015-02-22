6c217c3ccf98302f57c0f035f1a44f855e47b24b
Jimmy Walters <computerssuck93@hotmail.com>
2015-02-21 20:58:54 -0800

    Formatting the filtering results

--- p4-restaurant/js/scripts.js
+++ p4-restaurant/js/scripts.js
@@ -16,34 +16,46 @@ $(document).ready(function() {
       };
     })();
 
-    $("#input").keyup(function() {
+    $(".filter").click(function(){
 
-      // Delay function invoked to make sure user stopped typing
-      delay(function() {
-        inputText = $("#input").val().toLowerCase();
 
-        // Check to see if input field is empty
-        if ((inputText.length) > 0) {
-          $('.mix').each(function() {
+      if($(this).attr("data-filter") === "all"){
+       $(".container").mixItUp('changeLayout', 'block');
+      } else{
+        $('.container').mixItUp('changeLayout', 'inline-block');
+      }
+    });
+
+    // $("#input").keyup(function() {
+
+    //   // Delay function invoked to make sure user stopped typing
+    //   delay(function() {
+    //     inputText = $("#input").val().toLowerCase();
+
+    //     // Check to see if input field is empty
+    //     if ((inputText.length) > 0) {
+    //       $('.mix').each(function() {
             
-            $this = $("this");
+    //         $this = $("this");
 
-            // add item to be filtered out if input text matches items inside the title   
-            if ($(this).children('.title').text().toLowerCase().match(inputText)) {
-              $matching = $matching.add(this);
-            } else {
-              // removes any previously matched item
-              $matching = $matching.not(this);
-            }
-          });
+    //         // add item to be filtered out if input text matches items inside the title   
+    //         if ($(this).children('.title').text().toLowerCase().match(inputText)) {
+    //           $matching = $matching.add(this);
+    //         } else {
+    //           // removes any previously matched item
+    //           $matching = $matching.not(this);
+    //         }
+    //       });
 
-          $(".container").mixItUp('filter', $matching);
-        } else {
-          // resets the filter to show all item if input is empty
-          $(".container").mixItUp('filter', 'all');
-        }
+    //       $(".container").mixItUp('filter', $matching);
+    //     } else {
+    //       // resets the filter to show all item if input is empty
+    //       $(".container").mixItUp('filter', 'all');
+    //     }
 
-      }, 200); //END DELAY FUNCTION
+    //   }, 200); //END DELAY FUNCTION
 
-    }); //END INPUT KEYUP FUNCTION
+    // }); //END INPUT KEYUP FUNCTION
+
+  
 });
