0289a8c36e7017f9705085baed14e2e76a04077e
Jimmy Walters <computerssuck93@hotmail.com>
2015-02-27 19:08:05 -0800

    I have never felt like such a fucking failure :(
    I have been working on this for close to 20 hours STRAIGHT... There is
    just a lot of stuff i still wanna change.

--- p4-restaurant/js/scripts.js
+++ p4-restaurant/js/scripts.js
@@ -1,61 +1,89 @@
 $('.menu .item').tab();
 
-$(document).ready(function() {
 
-    $(".container").mixItUp();
+$(document).ready(function(){
 
-    var inputText;
-    var $matching = $();
+  $(".container").mixItUp();
 
-    // Delay function
-    var delay = (function() {
-      var timer = 0;
-      return function(callback, ms) {
-        clearTimeout(timer);
-        timer = setTimeout(callback, ms);
-      };
-    })();
+  var inputText;
+  var $matching = $();
 
-    $(".filter").click(function(){
+  // Delay function
+  var delay = (function(){
+    var timer = 0;
+
+    return function(callback, ms) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    };
+
+  })();
+
+  // $( ".filter" ).click( function() {
+
+  //     if ($(this).attr("data-filter") === "all"){
+  //        $(".container").mixItUp('changeLayout', 'block');
+  //     } else{
+  //        $(".container").mixItUp('changeLayout', 'inline-block');
+  //     }
+  //  });
 
 
-      if($(this).attr("data-filter") === "all"){
-       $(".container").mixItUp('changeLayout', 'block');
-      } else{
-        $('.container').mixItUp('changeLayout', 'inline-block');
-      }
-    });
 
-    // $("#input").keyup(function() {
+   /*
+   $("#input").keyup(function() {
 
-    //   // Delay function invoked to make sure user stopped typing
-    //   delay(function() {
-    //     inputText = $("#input").val().toLowerCase();
+       // Delay function invoked to make sure user stopped typing
+       delay(function() {
+         inputText = $("#input").val().toLowerCase();
 
-    //     // Check to see if input field is empty
-    //     if ((inputText.length) > 0) {
-    //       $('.mix').each(function() {
-            
-    //         $this = $("this");
+         // Check to see if input field is empty
+         if ((inputText.length) > 0) {
+           $('.mix').each(function() {
+             
+             $this = $("this");
 
-    //         // add item to be filtered out if input text matches items inside the title   
-    //         if ($(this).children('.title').text().toLowerCase().match(inputText)) {
-    //           $matching = $matching.add(this);
-    //         } else {
-    //           // removes any previously matched item
-    //           $matching = $matching.not(this);
-    //         }
-    //       });
+             // add item to be filtered out if input text matches items inside the title   
+             if ($(this).children('.title').text().toLowerCase().match(inputText)) {
+               $matching = $matching.add(this);
+             } else {
+               // removes any previously matched item
+               $matching = $matching.not(this);
+             }
+           });
 
-    //       $(".container").mixItUp('filter', $matching);
-    //     } else {
-    //       // resets the filter to show all item if input is empty
-    //       $(".container").mixItUp('filter', 'all');
-    //     }
+           $(".container").mixItUp('filter', $matching);
+         } else {
+           // resets the filter to show all item if input is empty
+           $(".container").mixItUp('filter', 'all');
+         }
 
-    //   }, 200); //END DELAY FUNCTION
+       }, 200); //END DELAY FUNCTION
 
-    // }); //END INPUT KEYUP FUNCTION
-
-  
+   }); //END INPUT KEYUP FUNCTION
+  */
 });
+
+
+
+$('.acc-head').collapser({
+
+  mode: 'block',
+  target: 'next',
+  effect: 'slide',
+  beforeShow: function(){
+
+    $('.acc-head').each(function(){
+
+      var dataVar = $(this).data('collapser');
+      if (dataVar) dataVar.hide($(this));
+    });
+  }
+});
+
+
+$(".raw p").append('<img src="img/raw.png">')
+$(".vege p").append('<img src="img/vege.png">')
+$(".spicy p").append('<img src="img/spicy.png">')
+$(".normal p").append('<img src="img/placeholder.gif">')
+
